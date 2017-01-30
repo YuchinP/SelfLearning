e9faec387ccf1800aa491e4628682302e5bdde34
YuchinP <yuchinspan@gmail.com>
2017-01-30 14:01:02 -0800

    Addition of Coursera Data Science
    Learning about RMySQL and how to query within R

---
+++ Coursera/RMySQL.R
@@ -0,0 +1,22 @@
+library(RMySQL)
+ucscDb <- dbConnect(MySQL(), user="genome",
+                    host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb, "show databases;"); dbDisconnect(ucscDb);
+result
+
+hg19 <- dbConnect(MySQL(), user="genome", db="hg19",
+                  host = "genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+allTables[1:5]
+
+dbListFields(hg19,"affyU133Plus2")
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+head(affyData)
+
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+affyMisSmall <- fetch(query, n=10); dbClearResult(query);
+dim(affyMisSmall)
